c2d73cc32d820b877457609043b441ee2be73a08
TanaShun0804 <shun.tanaka@hacomono.co.jp>
2021-03-08 10:54:58 +0900

    Header追加

---
+++ components/Header.vue
@@ -0,0 +1,45 @@
+<template>
+  <header>
+    <nav>
+      <span class="brand"><NuxtLink to="/">Hacomono Library</NuxtLink></span>
+      <ul>
+        <li><NuxtLink to="/">ダッシュボード</NuxtLink></li>
+        <li><NuxtLink to="/books">本一覧</NuxtLink></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  //
+}
+</script>
+
+<style lang="scss" scoped>
+header {
+  position: fixed;
+  top: 0;
+  width: 100%;
+  a {
+    color: white;
+  }
+}
+nav {
+  padding: 0 20px;
+  height: 60px;
+  display: flex;
+  background-color: rgb(27, 27, 27);
+  align-items: center;
+  justify-content: space-between;
+  .brand {
+    font-weight: bold;
+  }
+  ul {
+    display: flex;
+    li {
+      padding: 0 10px;
+    }
+  }
+}
+</style>
