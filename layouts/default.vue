0d62263ff26e353a82d2e4beb270ea9d85143c2c
TanaShun0804 <shun.tanaka@hacomono.co.jp>
2021-03-08 10:55:17 +0900

    Layoutファイル修正

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,14 @@
 <template>
-  <div>
-    <Nuxt />
+  <div class="layout-wrapper">
+    <Header />
+    <div class="layout-content">
+      <Nuxt />
+    </div>
+    <Footer />
   </div>
 </template>
 
-<style>
+<style lang="scss">
 html {
   font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
     Roboto, 'Helvetica Neue', Arial, sans-serif;
@@ -15,6 +19,19 @@ html {
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
+  background: rgb(243, 243, 243);
+}
+
+ul {
+  list-style: none;
+}
+
+a {
+  text-decoration: none;
+}
+
+a {
+  color: black;
 }
 
 *,
@@ -24,32 +41,13 @@ html {
   margin: 0;
 }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
+.layout-wrapper {
+  position: relative;
+  min-height: 100vh;
 }
 
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+.layout-content {
+  padding-top: 60px;
+  padding-bottom: 30px;
 }
 </style>
